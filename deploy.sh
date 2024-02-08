0067e301b582e99823f481f233e8cb2eaa0c3d47
root <root@ip-172-31-85-103.ec2.internal>
2024-02-08 06:36:36 +0000

    new commit

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ env.BRANCH_NAME -eq "dev" ]
+then
+        echo "push to dev repo"
+       
+       docker tag newimage:latest deepakramamoorthy/reactapplicationdev:2.0
+       docker push deepakramamoorthy/reactapplicationdev:2.0
+       
+        
+else
+        echo "push to prod repo"
+        
+	docker tag newimage:latest deepakramamoorthy/reactapplicationdev:2.0
+	docker push deepakramamoorthy/reactapplicationdev:2.0
+fi

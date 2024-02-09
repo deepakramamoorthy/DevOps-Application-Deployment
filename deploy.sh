3d1355f1cbad0981e035e2c25960d57d94866c82
deepakramamoorthy <151495447+deepakramamoorthy@users.noreply.github.com>
2024-02-09 22:46:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 branchName = sh(label: 'getBranchName', returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD').trim()
-println branchName
-if [  "$branchName" -eq "dev" ]
-then
-        echo "push to dev repo"
+               echo branchName
+               
+        if (branchName == "dev") {
+                    echo "push to dev repo"
+   
+       docker login -u deepakramamoorthy -p Dee9578461426@@@ 
+       docker tag newimage:latest deepakramamoorthy/reactapplicationdev:3.0
+       docker push deepakramamoorthy/reactapplicationdev:3.0
        
-       docker tag newimage:latest deepakramamoorthy/reactapplicationdev:2.0
-       docker push deepakramamoorthy/reactapplicationdev:2.0
-       
-        
-else
-        echo "push to prod repo"
-        
-	docker tag newimage:latest deepakramamoorthy/reactapplicationdev:2.0
-	docker push deepakramamoorthy/reactapplicationdev:2.0
-fi
+        } 
+	else
+	{
+             echo "push to prod repo"
+   
+	docker tag newimage:latest deepakramamoorthy/reactapplicationdev:3.0
+	docker push deepakramamoorthy/reactapplicationdev:3.0
+	}
